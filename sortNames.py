65b1295efb2acd3a5da5fff551d4a7a65c0b3674
FladChris <73495532+FladChris@users.noreply.github.com>
2023-05-09 12:46:27 +0200

    Update sortNames.py init
    Upload korrigiert

--- sortNames.py
+++ sortNames.py
@@ -5,14 +5,6 @@ input_list = str(text_datei.readline())
 
 
 
-
-'''
-Wenn ich soweit alles richtig verstanden habe, hat der Algorithmus eine Laufzeit von O(n log n), da einmal O(n) mit der Schleife 
-und dann einmal durch die rekursion O(log n) - Meinungen?. Schneller geht es ja eigentlich nicht oder?
-
-Ich hab mich beim Coden jetzt an den Algorithmen von den Folien orientiert. Deswegen auch zweigeteilt.
-'''
-
 def mergesort(input_list):
     if len(input_list) == 0 or len(input_list) == 1:  # Wenn 1 oder 0 direkte rückgabe (Basisfall)
         return input_list
@@ -21,12 +13,14 @@ def mergesort(input_list):
     right = mergesort(input_list[mid:])
     return(merge(left, right))  # Rückgabe der Liste an das system
 
+
 def merge(left, right):
     result = []  # hier bin ich unsicher, ist das richtig, dass man es als Liste zurück gibt? Ist ja sehr ähnlich einem Array?!
-    i = 0  # linker Index 
+    i = 0  # linker Index
     j = 0  # rechter Index
     while i < len(left) and j < len(right):
-        if left[i] <= right[j]:  # Prüfen ob das linke Element kleiner (Vorangiger ist)
+        # Prüfen ob das linke Element kleiner (Vorangiger ist)
+        if left[i] <= right[j]:
             result.append(left[i])  # linkes Element wird hinzugefügt
             i += 1
         else:
@@ -35,6 +29,7 @@ def merge(left, right):
     result.extend(left[i:])  # Rest anhängen linke Teilbaum
     result.extend(right[j:])  # Rest anhängen rechter Teilbaum
     return result
+
 
 def quicksort(input_list):
     if len(input_list) <= 1:  # Basisfall
@@ -49,4 +44,10 @@ def quicksort(input_list):
             right.append(input_list)  # hinzufügen wen ndas Element größer ist
     return(quicksort(left) + [akt_index] + quicksort(right))  # rekursiver aufruf
 
-print('Aufrufsbestätigung')
+
+if sys.argv[1] == '-merge':
+    print(mergesort(input_list))
+elif sys.argv[1] == '-quick':
+    print(quicksort(input_list))
+
+text_datei.close()
